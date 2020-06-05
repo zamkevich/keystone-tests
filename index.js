1511fa6b95cd593337b99a67623f7b068aba7698
Misha Zamkevich <uwwcom@gmail.com>
2020-06-05 13:58:49 +1000

    fix index

--- index.js
+++ index.js
@@ -2,18 +2,14 @@ const { Keystone } = require('@keystonejs/keystone');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
 const { MongooseAdapter } = require('@keystonejs/adapter-mongoose');
-
+const { Text } = require('@keystonejs/fields');
 
 const keystone = new Keystone({
-  name: 'test_content',
+  name: 'test',
   adapter: new MongooseAdapter(),
 });
 
-
-const { Content } = require('/Users/Z/Documents/Web-project/keystone/packages/field-content');
-
-keystone.createList('foo', { fields: { text: { type: Content }}});
-keystone.createList('bar', { fields: { text: { type: Content }}});
+keystone.createList('foo', { fields: { name: {type: Text }}});
 
 module.exports = {
   keystone,
